740e9b09104c178e02995389c93d59272b2388a9
bartkozlo <bartkozlo@gmail.com>
2021-10-04 16:27:16 +0200

    Add redux and preparing a project for it

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import Card from '../Card/Card';
 import Icon from '../Icon/Icon';
 import { settings } from '../../data/dataStore';
-import Creator from '../Creator/Creator';
+import Card from '../Card/Card';
+// import Creator from '../Creator/Creator';
 
 class Column extends React.Component {
-  state = {
-    cards: this.props.cards || [],
-  }
-
   static propTypes = {
     title: PropTypes.string.isRequired,
     cards: PropTypes.array,
@@ -21,36 +17,24 @@ class Column extends React.Component {
     icon: settings.defaultColumnIcon,
   }
 
-  addCard(title) {
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
-            title,
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
+    const {title, icon, cards} = this.props;
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}
+        <h3 className={styles.title}>{title}
           <span className={styles.icon}>
-            <Icon name={this.props.icon} />
+            <Icon name={icon} />
           </span>
         </h3>
         <div className={styles.cards}>
-          {this.state.cards.map(({key, ...cardProps}) => (
-            <Card key={key} {...cardProps} />
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
           ))}
+
         </div>
-        <div className={styles.creator}>
+        {/* <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div>
+        </div> */}
       </section>
     );
   }
