ebf71d24620dc04973627b9bd153a1e465fca50a
bartkozlo <bartkozlo@gmail.com>
2021-10-18 16:46:52 +0200

    Modify search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,67 +2,48 @@ import React from 'react';
 import styles from './Search.scss';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
-import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon';
-import Container from '../Container/Container';
+import { settings } from '../../data/dataStore';
+import { withRouter } from 'react-router';
 
 class Search extends React.Component {
-  static propTypes = {
-    text: PropTypes.string,
-    searchString: PropTypes.string,
-    changeSearchString: PropTypes.func,
-    countVisible: PropTypes.number,
-    countAll: PropTypes.number,
-  }
-
-  static defaultProps = {
-    text: settings.search.defaultText,
-  }
 
   state = {
-    value: this.props.searchString,
+    value: '',
+    visibleButtons: false,
   }
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({
       value: event.target.value,
       visibleButtons: event.target.value.length > 0,
     });
   }
 
-  handleOK(){
-    this.props.changeSearchString(this.state.value);
-  }
-
-  componentDidUpdate(prevProps){
-    if(this.props.searchString != prevProps.searchString){
-      this.setState({value: this.props.searchString});
-    }
+  handleOK() {
+    this.props.history.push(`/search/${this.state.value}`);
   }
 
   render() {
-    const {text, countVisible, countAll} = this.props;
-    const {value} = this.state;
-    const {icon} = settings.search;
+    const { value } = this.state;
+    const { icon } = settings.search;
     return (
-      <Container>
-        <div className={styles.component}>
-          <input
-            type='text'
-            placeholder={text}
-            value={value}
-            onChange={event => this.handleChange(event)}
-          />
-          <div className={styles.buttons}>
-            <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
-          </div>
-          <div>
-            { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
-          </div>
+      <div className={styles.component}>
+        <input
+          type='text'
+          value={value}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <div className={styles.buttons}>
+          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
         </div>
-      </Container>
+      </div>
     );
   }
 }
 
-export default Search;
+Search.propTypes = {
+  history: PropTypes.object,
+};
+
+export default withRouter(Search);
--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,14 +1,20 @@
 import shortid from 'shortid';
 
-export const getCardsForColumn = ({ cards, searchString }, columnId) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
+// selectors
+export const getCardsForColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId && new RegExp('i').test(card.title));
 
+export const getCardsForSearchResults = ({ cards }, searchPhrase) => cards.filter((card) => new RegExp(searchPhrase, 'i').test(card.title));
+// action name creator
 const reducerName = 'cards';
 const createActionName = name => `app/${reducerName}/${name}`;
 
+// action types
 export const ADD_CARD = createActionName('ADD_CARD');
 
+// action creators
 export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
 
+// reducer
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
     case ADD_CARD:
