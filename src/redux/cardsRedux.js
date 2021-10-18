1e3a69d7c7e106e5477d7ddfc0dd08b18bb8db69
bartkozlo <bartkozlo@gmail.com>
2021-10-18 19:35:37 +0200

    Fix cardsRedux.js

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId && new RegExp('i').test(card.title));
+export const getCardsForColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId);
 
 export const getCardsForSearchResults = ({ cards }, searchPhrase) => cards.filter((card) => new RegExp(searchPhrase, 'i').test(card.title));
 // action name creator
